e5d31b5e77f10e6f633d66d502575decc41b6363
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-15 18:55:47 +0100

    profiele endpoint for non auth user

--- src/components/profiles/profile.js
+++ src/components/profiles/profile.js
@@ -22,7 +22,22 @@ const getUser = async (req, res) => {
 }
 
 const getOtherUsers = async (req, res) => {
-
+  try {
+    let user = await User.findOne({username: req.username})
+    journalIds = user.journals
+    user.journals = []
+    for (let journalId of journalIds) {
+      let journal = await Journal.findById(journalId)
+      user.journals.push(journal)
+    }
+    user = user.toObject()
+    delete user.password;
+    let resp = new ResponseObject(200, `Successfully retrieved data for user -${user.id}`, "ok", {user})
+    res.status(resp.statusCode).json({resp})
+  } catch (error) {
+    let resp = new ResponseObject(500, error.message, 'error', null)
+    res.status(resp.statusCode).json({resp})
+  }
 }
 
 module.exports.getUser = getUser
--- src/components/profiles/profileRoutes.js
+++ src/components/profiles/profileRoutes.js
@@ -5,5 +5,6 @@ const { getUser, getOtherUsers } = require('./profile');
 
 router.get('/', getUser)
 router.get('/profile', getOtherUsers)
+router.put('/', updateProfile)
 
 module.exports = router;
