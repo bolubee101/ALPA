c1ab80aaa0129432dc4e7ebb8534e3d83c31f4d9
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-16 14:23:00 +0100

    Data validation

--- src/components/profiles/profile.js
+++ src/components/profiles/profile.js
@@ -89,9 +89,14 @@ const updateProfile = async (req, res) => {
 const changeAvatar = async (req, res) => {
   try {
     let user = await User.findOne({email: req.email})
-    let {
-      avatar
-    } = req.files
+    let avatar= req.files.avatar
+    console.log(avatar)
+    if (!['image/png', 'image/jpeg'].includes(avatar[0].mimetype)) {
+      let response = new ResponseObject(400, "You need to upload an image", 'Bad data', null);
+      res.status(response.statusCode);
+      delete response.statusCode;
+      return res.json(response);
+    }
     let avatar_link = await uploadFile(avatar[0])
     console.log('avatar link', avatar_link)
     if (avatar_link.statusCode) return res.status(avatar_link.statusCode).json({avatar_link})
