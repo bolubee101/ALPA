f694a1e4278ec3ebfb5c4fe19321396663f7c9e4
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-05-19 09:35:59 +0100

    .

--- src/components/verifyUser.js
+++ src/components/verifyUser.js
@@ -5,13 +5,21 @@ const verifyToken = async (req, res, next) => {
   const tokenHeader = req.headers['authorization']
   if (typeof tokenHeader !== 'undefined') {
       const token = tokenHeader.split(' ')[1]
-      const {email} = await verify(token, jwtsecret)
-      req.email = email
-      next()
+      try {
+        const {email} = await verify(token, jwtsecret)
+        req.email = email
+        next()
+      } catch (error) {
+        let response = new ResponseObject(400, "Unable to verify user", 'Unauthorized', null);
+        res.status(response.statusCode);
+        delete response.statusCode;
+        return res.json(response);
+      }
   } else {
-      res.status(401).json({
-          error: 'Unauthorized'
-      })
+    let response = new ResponseObject(401, "You are not authorized", 'Unauthorized', null);
+    res.status(response.statusCode);
+    delete response.statusCode;
+    return res.json(response);
   }
 }
 
